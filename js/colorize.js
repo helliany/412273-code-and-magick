2a89b54341eaace9caa6f430a81b06923e1baf2a
helliany <ol.yurmanova@gmail.com>
2018-06-29 22:19:54 +0300

    Выполнила 6 задание

--- js/colorize.js
+++ js/colorize.js
@@ -2,10 +2,10 @@
 'use strict';
 
 (function () {
-  var WIZARD_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
+  var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
     'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
-  var WIZARD_FIREBALL = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+  var WIZARD_FIREBALLS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
   var setup = document.querySelector('.setup');
   var wizardCoat = setup.querySelector('.wizard-coat');
@@ -27,8 +27,8 @@
     });
   };
 
-  colorize(wizardCoat, WIZARD_COAT, inputCoat);
+  colorize(wizardCoat, WIZARD_COATS, inputCoat);
   colorize(wizardEyes, WIZARD_EYES, inputEyes);
-  colorize(wizardFireball, WIZARD_FIREBALL, inputFireball);
+  colorize(wizardFireball, WIZARD_FIREBALLS, inputFireball);
 
 })();
